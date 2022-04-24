2f6ef5e547db713b82b4d914b7d335452c729cb6
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2022-04-24 13:39:24 +0900

    基礎の追加 - Add plugin bases.

---
+++ src/boymelancholy/dropitem3d/DropItem3D.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace boymelancholy\dropitem3d;
+
+use pocketmine\plugin\PluginBase;
+
+class DropItem3D extends PluginBase
+{
+    public function onEnable() : void
+    {
+
+    }
+
+    public function onDisable() : void
+    {
+
+    }
+}
