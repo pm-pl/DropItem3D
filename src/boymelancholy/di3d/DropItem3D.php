3574c9f4dca5aeebfae30558c593372373070893
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2022-04-24 16:00:21 +0900

    Configの登録箇所を書き換えた - Rewrite config registration.

--- src/boymelancholy/di3d/DropItem3D.php
+++ src/boymelancholy/di3d/DropItem3D.php
@@ -64,8 +64,10 @@ class DropItem3D extends PluginBase
 
     private function registerConfig()
     {
-        @mkdir($this->getDataFolder());
-        $this->saveResource("config.yml");
+        if (!file_exists($this->getDataFolder() . "config.yml")) {
+            @mkdir($this->getDataFolder());
+            $this->saveResource("config.yml");
+        }
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }
 
