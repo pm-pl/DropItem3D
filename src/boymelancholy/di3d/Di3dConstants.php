835f26abdac2cf5b4937a5d89ddae99ddc4384a0
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2022-04-24 15:43:50 +0900

    アーマーを装着させる処理の削除 - Delete process to equip armor to player.

--- src/boymelancholy/di3d/Di3dConstants.php
+++ src/boymelancholy/di3d/Di3dConstants.php
@@ -9,5 +9,13 @@ class Di3dConstants
     const CONFIG_DROP_ITEM_PICKUP = 'drop-item-pickup';
     const CONFIG_ARMOR_DROP_STYLE = 'armor-drop-style';
     const CONFIG_ARMOR_EQUIP_INSTANTLY = 'armor-equip-instantly';
-    const CONFIG_ARMOR_EXCHANGE_EQUIP = 'armor-exchange-equip';
+
+    const PICK_UP_LIKE_VANILLA = 0;
+    const PICK_UP_BY_INTERACT = 1;
+
+    const DROP_STYLE_ARMOR = 0;
+    const DROP_STYLE_ITEM = 1;
+
+    const PUT_AWAY_ARMOR_TO_EQUIP = 0;
+    const PUT_AWAY_ARMOR_TO_INVENTORY = 1;
 }
