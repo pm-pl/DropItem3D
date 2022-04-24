b6ce2d912323dcd873f2093f6511956f3970df38
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2022-04-24 14:28:16 +0900

    立体アイテムのオブジェクト追加 - Add RealisticDropItem

--- src/boymelancholy/di3d/DropItem3D.php
+++ src/boymelancholy/di3d/DropItem3D.php
@@ -4,25 +4,66 @@ declare(strict_types=1);
 
 namespace boymelancholy\di3d;
 
+use boymelancholy\di3d\entity\object\RealisticDropItem;
+use pocketmine\data\bedrock\EntityLegacyIds;
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\entity\EntityFactory;
+use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\world\World;
 
 class DropItem3D extends PluginBase
 {
+    /** @var Config */
+    private Config $config;
+
     public function onEnable() : void
     {
+        self::$instance = $this;
 
+        $this->registerConfig();
+        $this->registerObject();
+        $this->registerListener();
     }
 
-    public function onDisable() : void
+    public function getConfig() : Config
     {
-
+        return $this->config;
     }
 
-    private function registerConfig() : void
+    private function registerListener()
+    {
+        $listeners = [];
+        foreach ($listeners as $listener) {
+            $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+        }
+    }
+
+    private function registerObject()
+    {
+        EntityFactory::getInstance()->register(
+            RealisticDropItem::class,
+            function(World $world, CompoundTag $nbt) : RealisticDropItem {
+                return new RealisticDropItem(EntityDataHelper::parseLocation($nbt, $world), null, $nbt);
+            },
+            ["RealisticDropItem", "minecraft:armor_stand"],
+            EntityLegacyIds::ARMOR_STAND
+        );
+    }
+
+    private function registerConfig()
     {
         @mkdir($this->getDataFolder());
         $this->saveResource('config.yml');
         $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
+    }
+
+
+    private static self $instance;
+
+    public static function getInstance() : self
+    {
+        return self::$instance;
     }
 }
---
+++ src/boymelancholy/di3d/entity/object/RealisticDropItem.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace boymelancholy\di3d\entity\object;
+
+use pocketmine\entity\EntitySizeInfo;
+use pocketmine\entity\Living;
+use pocketmine\inventory\ArmorInventory;
+use pocketmine\item\Armor;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\convert\TypeConverter;
+use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
+use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
+
+class RealisticDropItem extends Living
+{
+    /** @var Item|null */
+    private ?Item $item = null;
+
+    protected function getInitialSizeInfo() : EntitySizeInfo
+    {
+        return new EntitySizeInfo(1.8, 0.6, 1.62);
+    }
+
+    public static function getNetworkTypeId() : string
+    {
+        return EntityIds::ARMOR_STAND;
+    }
+
+    public function getName() : string
+    {
+        return "RealisticDropItem";
+    }
+
+    public function initEntity(CompoundTag $nbt) : void
+    {
+        parent::initEntity($nbt);
+
+        $this->setHasGravity(false);
+        $this->setInvisible();
+        $this->setImmobile();
+    }
+
+    /**
+     * Get an object's item.
+     * @return Item|null
+     */
+    public function getItem() : ?Item
+    {
+        return $this->item;
+    }
+
+    /**
+     * Make object have the armor.
+     * @param Armor $item
+     */
+    public function setEquippableItem(Armor $item)
+    {
+        $this->item = $item;
+        $this->getArmorInventory()->setItem($item->getArmorSlot(), $item);
+
+        $deltaVector = Vector3::zero();
+        $deltaVector->y -= match ($item->getArmorSlot()) {
+            ArmorInventory::SLOT_HEAD => 1.25,
+            ArmorInventory::SLOT_CHEST => 0.75,
+            ArmorInventory::SLOT_LEGS => 0.35,
+            ArmorInventory::SLOT_FEET => 0.1,
+        };
+        $yaw = mt_rand(0, 360);
+        $pitch = 0.0;
+        $this->teleport($this->getLocation()->addVector($deltaVector), $yaw, $pitch);
+
+        $this->scheduleUpdate();
+    }
+
+    /**
+     * Make object have the item.
+     * @param Item $item
+     */
+    public function setHeldItem(Item $item)
+    {
+        $this->item = $item;
+        $this->getNetworkProperties()->setInt(EntityMetadataProperties::ARMOR_STAND_POSE_INDEX, 8);
+
+        $pk = new MobEquipmentPacket();
+        $pk->actorRuntimeId = $this->getId();
+        $pk->item = ItemStackWrapper::legacy(TypeConverter::getInstance()->coreItemStackToNet($item));
+        $pk->hotbarSlot = 0;
+        $pk->inventorySlot = 0;
+        foreach ($this->getWorld()->getPlayers() as $player) {
+            $player->getNetworkSession()->sendDataPacket($pk);
+        }
+
+        $deltaVector = Vector3::zero();
+        $deltaVector->y -= ($this->getEyeHeight() - 0.25);
+        $yaw = mt_rand(0, 360);
+        $pitch = 0.0;
+        $this->teleport($this->getLocation()->addVector($deltaVector), $yaw, $pitch);
+
+        $this->scheduleUpdate();
+    }
+
+
+}
