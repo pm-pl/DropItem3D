062f9333e56765dbf50bb18f3d2c8c2f44902069
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2022-04-24 14:27:49 +0900

    定数 - Constants.

---
+++ src/boymelancholy/di3d/Di3dConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace boymelancholy\di3d;
+
+class Di3dConstants
+{
+    const CONFIG_DROP_ITEM_PICKUP = 'drop-item-pickup';
+    const CONFIG_ARMOR_DROP_STYLE = 'armor-drop-style';
+    const CONFIG_ARMOR_EQUIP_INSTANTLY = 'armor-equip-instantly';
+    const CONFIG_ARMOR_EXCHANGE_EQUIP = 'armor-exchange-equip';
+}
